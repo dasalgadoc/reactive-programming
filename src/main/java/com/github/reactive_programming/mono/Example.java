33138e404b2aa05e2b225b5600b6d0ccdfa201ef
Diego Salgado <dasalgadoc@unal.edu.co>
2024-07-15 00:01:16 -0500

    examples on mono

--- src/main/java/com/github/reactive_programming/mono/Example.java
+++ src/main/java/com/github/reactive_programming/mono/Example.java
@@ -11,12 +11,44 @@ import reactor.core.publisher.Mono;
 @RequestMapping
 public class Example {
 
-  @GetMapping("/mono/example")
-  public String example() {
+  @GetMapping("/mono/example/1")
+  public String exampleJustAndSubscribe() {
     List<String> message = new ArrayList<>();
 
+    // Mono just: Creates a new Mono that emits the specified item
     Mono<String> mono = Mono.just("Hello, world!");
     mono.subscribe(message::add);
+
+    return message.toString();
+  }
+
+  @GetMapping("/mono/example/2")
+  public String exampleMonoWithThreeParameters() {
+    List<String> message = new ArrayList<>();
+
+    Mono<String> mono = Mono.just("Hello, world Example 2!");
+
+    // Mono subscribe with three parameters
+    // 1. onNext: Called when a value is emitted by the Mono
+    // 2. onError: Called when an error occurs
+    // 3. onComplete: Called when the Mono completes without emitting a value
+    mono.subscribe(message::add, Throwable::printStackTrace, () -> message.add("Completed"));
+
+    return message.toString();
+  }
+
+  @GetMapping("/mono/example/3")
+  public String exampleMonoAndError() {
+    List<String> message = new ArrayList<>();
+
+    Mono<String> mono =
+        Mono.fromSupplier(
+            () -> {
+              throw new RuntimeException("Error on Mono Example 3!");
+            });
+
+    mono.subscribe(
+        message::add, error -> message.add(error.getMessage()), () -> message.add("Completed"));
 
     return message.toString();
   }
